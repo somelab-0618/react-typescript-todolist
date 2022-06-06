dcdc9eb495f11b321f74fdb07b511a1d96cf7e89
RyosukeSomeya <rx.0.0618@gmail.com>
2022-06-06 22:36:24 +0900

    タスク追加機能実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import { FormControl, TextField } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+import { db } from './firebase';
+import { AddToPhotos } from '@material-ui/icons';
+
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState([{ id: '', title: '' }]);
+  const [input, setInput] = useState('');
+
+  useEffect(() => {
+    const unSub = db.collection('tasks').onSnapshot((snapshot) => {
+      setTasks(
+        snapshot.docs.map((doc) => ({ id: doc.id, title: doc.data().title }))
+      );
+    });
+    return () => unSub();
+  }, []);
+
+  const newTask = (e: React.MouseEvent<HTMLButtonElement>) => {
+    db.collection('tasks').add({ title: input });
+    setInput('');
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Todo App By React-Typescript-Firebase</h1>
+      <FormControl>
+        <TextField
+          InputLabelProps={{ shrink: true }}
+          value={input}
+          label='new task?'
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInput(e.target.value)
+          }
+        />
+      </FormControl>
+      <button disabled={!input} onClick={newTask}>
+        <AddToPhotos />
+      </button>
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.title}</h3>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
